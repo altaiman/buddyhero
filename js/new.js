b69cf8cdaf6c5902784d27eb4041bd59f345305d
Roman <altaiman@gmail.com>
2018-12-08 14:50:39 +0700

    update

--- js/new.js
+++ js/new.js
@@ -5,15 +5,24 @@
   // modal
   $('.buddy__more').on('click', function() {
     var title = $(this).parent().find('.buddy__name').text(),
-        content = $(this).parent().find('.more').html();
+        content = $(this).parent().find('.more').html(),
+        id = $(this).closest('[data-buddy]').data('buddy');
 
+
+    $('.modal_more').data('buddy', id);
     $('.modal_more .modal__title').empty().text(title);
     $('.modal_more .modal__content').empty().html(content);
 
     $('.modal_more').iziModal('open');
   });
 
-  $('.buddy__get').on('click', function() {
+  $('.buddy__get, .get').on('click', function() {
+    $('.modal').iziModal('close');
+
+    var action = ($(this).closest('[data-buddy]').data('buddy')) ? 'sendmail.php' : 'joinmail.php'
+    
+    $('.modal_get .form').attr('action', action);
+
     $('.modal_get').iziModal('open');
   });
 
@@ -39,5 +48,13 @@
     setTimeout(function() {
       $('.modal_zoom').iziModal('close');
     }, 3000)
+  });
+
+  $('.buddy-big').on('click', function(e) {
+    e.preventDefault();
+
+    var id = $(this).data('buddy');
+
+    $('.buddy[data-buddy='+id+']').find('.buddy__more').trigger('click');
   });
 })();
